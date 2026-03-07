b0bd89470d5b209043f0c40f62139240726739a3
madebynanditaaa <nandita.singh2004@gmail.com>
2026-03-07 23:01:15 +0530

    fetch data from backend

--- ecommerce/src/pages/HomePage.jsx
+++ ecommerce/src/pages/HomePage.jsx
@@ -1,10 +1,17 @@
 import { Header } from "../components/Header";
 import { products } from "../../starting-code/data/products";
 
-
 import "./HomePage.css";
 
 export function HomePage() {
+  fetch("http://localhost:3000/api/products")
+    .then((response) => {
+      return response.json();
+    })
+    .then((data) => {
+      console.log(data);
+      // console.log(response);
+    });
   return (
     <>
       <title>Ecommerce Project</title>
