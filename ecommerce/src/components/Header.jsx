8827d6aeb858c785a0ac479505755248f3d1a300
madebynanditaaa <nandita.singh2004@gmail.com>
2026-03-08 16:14:04 +0530

    get cartfrom backend

--- ecommerce/src/components/Header.jsx
+++ ecommerce/src/components/Header.jsx
@@ -1,35 +1,38 @@
-import './Header.css';
+import "./Header.css";
 
-export function Header()
-{
-    return(
-        <div className="header">
-        <div className="left-section">
-          <a href="/" className="header-link">
-            <img className="logo" src="../assets/logo-white.png" />
-            <img className="mobile-logo" src="../assets/mobile-logo-white.png" />
-          </a>
-        </div>
-
-        <div className="middle-section">
-          <input className="search-bar" type="text" placeholder="Search" />
-
-          <button className="search-button">
-            <img className="search-icon" src="../assets/icons/search-icon.png" />
-          </button>
-        </div>
-
-        <div className="right-section">
-          <a className="orders-link header-link" href="/orders">
-            <span className="orders-text">Orders</span>
-          </a>
-
-          <a className="cart-link header-link" href="/checkout">
-            <img className="cart-icon" src="../assets/icons/cart-icon.png" />
-            <div className="cart-quantity">3</div>
-            <div className="cart-text">Cart</div>
-          </a>
-        </div>
+export function Header({ cart }) {
+  let totalQuantity = 0;
+  cart.forEach((cartItem) => {
+    totalQuantity += cartItem.quantity;
+  });
+  return (
+    <div className="header">
+      <div className="left-section">
+        <a href="/" className="header-link">
+          <img className="logo" src="../assets/logo-white.png" />
+          <img className="mobile-logo" src="../assets/mobile-logo-white.png" />
+        </a>
       </div>
-    );
+
+      <div className="middle-section">
+        <input className="search-bar" type="text" placeholder="Search" />
+
+        <button className="search-button">
+          <img className="search-icon" src="../assets/icons/search-icon.png" />
+        </button>
+      </div>
+
+      <div className="right-section">
+        <a className="orders-link header-link" href="/orders">
+          <span className="orders-text">Orders</span>
+        </a>
+
+        <a className="cart-link header-link" href="/checkout">
+          <img className="cart-icon" src="../assets/icons/cart-icon.png" />
+          <div className="cart-quantity">{totalQuantity}</div>
+          <div className="cart-text">Cart</div>
+        </a>
+      </div>
+    </div>
+  );
 }
--- ecommerce/src/pages/HomePage.jsx
+++ ecommerce/src/pages/HomePage.jsx
@@ -1,20 +1,26 @@
 import axios from "axios";
 import { useEffect, useState } from "react";
-import { Header } from "../components/Header"; 
+import { Header } from "../components/Header";
 import "./HomePage.css";
 
 export function HomePage() {
-  const [products, setProducts]= useState([]);
+  const [products, setProducts] = useState([]);
+  const [cart, setCart] = useState([]);
+  
   useEffect(() => {
     axios.get("http://localhost:3000/api/products").then((response) => {
       setProducts(response.data);
+    });
+    axios.get("http://localhost:3000/api/cart-items").then((response) => {
+
+    setCart(response.data);
     });
   }, []);
 
   return (
     <>
       <title>Ecommerce Project</title>
-      <Header />
+      <Header cart={cart} />
       <div className="home-page">
         <div className="products-grid">
           {products.map((products) => {
